f2758de1a468d91372040cb75aa57808e02308b2
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2025-07-07 22:31:20 +0600

    while & for loop

--- index.php
+++ index.php
@@ -10,39 +10,28 @@
 <body>
     <?php
     $car = "bmw";
-    echo "My favorite car is a " . $car;
+    echo "My favorite car is a " . $car . "<br>" . "<br>";
+
+
+
     /*
     roknuzzaman
      roknuZzaman
      roknu_zzaman
      */
     ## // variables declear and var_dump check 
-    $age = 20.5;
-    $name = 'rased';
-    $age1 = 30;
-    $name = 'bulbul';
-    // var_dump($age1);
-    // var_dump($age);
-    // var_dump($name);
     
-    // class of php
-    class Phone
-    {
-        var $device;
-        function mobileDevice($number)
-        {
-            global $device;
-            $device = $number;
-            // echo "this is a $device <br>";
-        }
-    }
-    ## // class define
-    $xiaomi = new Phone;
-    $xiaomi->mobileDevice(" redmi note 10 lite");
-    $Realmi = new Phone;
-    $Realmi->mobileDevice(" c 5 i");
-    $akhi = 8;
-    $badol = 9;
+    // $age = 20.5;
+    // $name = 'rased';
+    // $age1 = 30;
+    // $name = 'bulbul';
+    // ## // class define
+    // $xiaomi = new Phone;
+    // // $xiaomi->mobileDevice(" redmi note 10 lite");
+    // $Realmi = new Phone;
+    // $Realmi->mobileDevice(" c 5 i");
+    // $akhi = 8;
+    // $badol = 9;
     // echo $akhi + $badol;
     
     $set = str_replace('badol', 'akhi', 'he is siter in badol');
@@ -51,20 +40,25 @@
     $x = 6;
     $y = 9;
     ## // const variable decaliter
+    
     define('phones', [
         'xiaomi',
         'nokia',
         'oneplus'
     ]);
+
     // echo (phones[2]);
     // var_dump(phones);
     ## // php function on const variable
+    
     function test()
     {
         // echo (phones[1]);
     }
     ;
+
     test();
+
     // echo $x + $y . '<br>';
     
     ##// php comparison operator
@@ -79,10 +73,11 @@
     // $xx = 500;
     // $yy = 50;
     // echo($xx > $yy);    
-    
     ## // Php if else statements
     
+
     $date = date('H');
+
     // if ($date == 20) {
     //     echo "It's twenty";
     // }else{
@@ -105,77 +100,32 @@
     // }
     ## switch in php
     
+
     switch ("red") {
         case 'green':
-            // echo 'my favorite color is red';
+            echo 'my favorite color is red';
             break;
         case 'blue':
-            // echo 'my favorite color is also red' . '<br >';
+            echo 'my favorite color is also red' . '<br >';
             break;
         default:
-            // echo 'I have not favorite color' . '<br >';
+            echo 'I have not favorite color' . '<br >';
             break;
     }
 
-    ## // php loop
-    // while loop
-    $wl = 1;
-    // while ($wl <= 10) {
-    //     echo 'this is' . $wl . '<br>';
-    //     $wl++;
-    // }
-    // do while loop
-    $dl = 1;
-    // do {
-    //     echo 'this is ' . $dl . '<br>';
-    //     $dl++;
-    // } while ($dl <= 10);
+    // Loop in php
     
-    //  for loop
-    // for($fl = 1; $fl <=10; $fl++){
-    //     echo 'this is for fl ' . $fl . '<br>';
-    // }
-    
-    ## date format
-    // echo date('Y-m-d H:i:s');
-    
-    ##// class constructor 
-    class Car
-    {
-        public $color;
-        function __construct($color)
-        {
-            $this->color = $color;
-            // echo "this car is " . $this->color . "<br>";
-        }
-        function carInfo($fcolor)
-        {
-            $this->color = $fcolor;
-            // echo "this car is " . $this->color . "<br>";
-        }
-    }
-    $bmw = new Car("");
-    // echo $bmw->carInfo('red');
-    // echo($bmw->carInfo())     ;
-    $mercedes = new Car("");
-    // echo $mercedes->carInfo("blue");
-    // var_dump($bmw);
-    
-    // oop trait
-    
-    trait Bank
-    {
-        public function deposit($amount)
-        {
-            echo "depositing into bank";
-        }
-        public function withdraw($amount)
-        {
-            echo "withdrawing $amount";
-        }
+    // while Loop
+    $i = 0;
+    while ($i <= 10) {
+        // echo "The number is: $i <br>";
+        $i++;
     }
 
-
+    // for Loop
+    for ($i = 1; $i <= 50; $i++) {
+        echo  $i. ":- Sumaiya I am Sorry Tumi Sobtheke Valo Student"."<br>";
+    }
     ?>
 </body>
 
